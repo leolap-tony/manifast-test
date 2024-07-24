0bfd5c16cb8276b3f91914634a5db07aaf23d4b7
Tony <tony@MacBook-Pro.local>
2024-07-24 11:16:26 +0900

    feat: 캐시 시간 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    staleTimes: {
+      dynamic: 600,
+      static: 600,
+    }
+  },
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
