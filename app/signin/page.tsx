38f088be391cdac414e09622c0db759ce0697d06
Tony <tony@MacBook-Pro.local>
2024-07-30 10:25:06 +0900

    fix : custom signin 페이지 서버컴포넌트로 변경

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,6 @@
-"use client";
-import { signIn } from "next-auth/react";
+// "use client";
+// import { signIn } from "next-auth/react";
+import { signIn } from "@/auth";
 import Image from "next/image";
 import Google from "@/public/google.svg";
 import { Button } from "@/components/elements/Button";
@@ -7,7 +8,10 @@ import { Button } from "@/components/elements/Button";
 export default function Signin() {
   return (
     <div className="w-full h-[100vh] flex">
-      <div className="absolute inset-0 w-[400px] h-fit border rounded-lg place-self-center place-content-center p-6 flex flex-col gap-8 items-center bg-background drop-shadow-md">
+      <form className="absolute inset-0 w-[400px] h-fit border rounded-lg place-self-center place-content-center p-6 flex flex-col gap-8 items-center bg-background drop-shadow-md" action={async ()=>{
+        'use server'
+        await signIn('google',{redirectTo:'/'})
+      }}>
         <div className="flex flex-col items-center gap-2">
           <h1 className="text-text-title text-title-lg">로그인</h1>
           <p className="text-body-md-n text-text-sub">
@@ -16,7 +20,7 @@ export default function Signin() {
         </div>
         <Button
           variant="outline"
-          onClick={() => signIn("google", { redirectTo: "/dashboard" })}
+          // onClick={() => signIn("google", { redirectTo: "/dashboard" })}
           className="w-full"
         >
           <div className="flex flex-row gap-2 items-center">
@@ -24,7 +28,7 @@ export default function Signin() {
             <span className="text-title-sm">구글 로그인</span>
           </div>
         </Button>
-      </div>
+      </form>
     </div>
   );
 }
