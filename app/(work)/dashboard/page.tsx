770af5af46fa7c3d07d9b35743546476a3399f00
sheeppattern <monhee96@gmail.com>
2024-07-31 10:34:43 +0900

    style : 첫화면 대시보드 적용

--- app/(work)/dashboard/page.tsx
+++ app/(work)/dashboard/page.tsx
@@ -15,13 +15,15 @@ import {
 import DailyProjectTable from "@/components/DailyProjectTable";
 import { ProjectWithTasks } from "@/types/queryInterface";
 import { group } from "console";
+import SupplyDashboard from "@/components/SupplyDashboard";
+import DemandDashboard from "@/components/DemandDashboard";
 
 // import { createReport, getReports } from "./actions";
 
 export default async function page() {
   const session = await auth();
 
-  const offset = 1000 * 60 * 60 * 9;
+  /*const offset = 1000 * 60 * 60 * 9;
   const koreaNow = new Date(new Date().getTime() + offset);
   const startOfToday = new Date(koreaNow.setHours(0, 0, 0, 0));
   const endOfToday = new Date(koreaNow.setHours(23, 59, 59, 999));
@@ -49,18 +51,23 @@ export default async function page() {
   });
 
   const myProjects = user?.managementGroups.flatMap((group) => group.projects);
-  const myTasks = user?.tasks;
+  const myTasks = user?.tasks;*/
   return (
     <main className="page-contents">
       {/*<pre>{JSON.stringify(myProjects, null, 2)}</pre>*/}
       <Header type="dashboard" />
       <section className="page-section">
-        <div className="w-full grid grid-cols-3 p-6 gap-4">
+        {/*<div className="w-full grid grid-cols-3 p-6 gap-4">
           <SummaryCard type="project" value={0}></SummaryCard>
           <SummaryCard type="inputRate" value={[0, 0]}></SummaryCard>
           <SummaryCard type="project" value={0}></SummaryCard>
         </div>
-        {<DailyProjectTable projects={myProjects as ProjectWithTasks[]} />}
+        <DailyProjectTable projects={myProjects as ProjectWithTasks[]} />*/}
+        {session?.user?.role == "WORKER" || session?.user.role == "MANAGER" ? (
+          <SupplyDashboard />
+        ) : (
+          <DemandDashboard />
+        )}
       </section>
     </main>
   );
